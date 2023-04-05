fe0b626de9cfd2ffb8312c5402fed699c3bd54a0
laurentnamasaka <laurentnamasaka@gmail.com>
2023-04-05 10:11:05 +0300

    first commit

--- basic.py
+++ basic.py
@@ -1,92 +1,173 @@
+######################################################################
+# IMPORTS
+######################################################################
+import string
 
+######################################################################
+# CONSTANTS
+######################################################################
 DIGITS = '0123456789'
+LETTERS = string.ascii_letters
+LETTERS_DIGITS = LETTERS + DIGITS
 
-#ERRORS
-class Error:        
-    def __init__(self,error_name, details):
+######################################################################
+# ERRORS
+######################################################################
+
+
+class Error:
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
 
     def as_string(self):
         result = f'{self.error_name}: {self.details}'
+        result += f' File {self.pos_start.file_name}, line {self.pos_start.line + 1}'
         return result
 
+
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
-    #class ExpectedCharError(Error):
-     #   def __init__(self, pos_start, pos_end, details):
-      #      super().__init__(pos_start, pos_end, 'Expected Character', details)
 
-#class Error:
- #   def __init__(self, pos_start, pos_end, error_name, details):
-  #      self.pos_start = pos_start
-   #     self.pos_end = pos_end
-    #    self.error_name = error_name
-     #   self.details = details
+class ExpectedCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Expected Character', details)
 
-    #def as_string(self):
-     #   result  = f'{self.error_name}: {self.details}'
-      #  result += f' File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
-       # return result
 
-#class IllegalCharError(Error):
- #   def __init__(self, pos_start, pos_end, details):
-  #      super().__init__(pos_start, pos_end, 'Illegal Character', details)
+######################################################################
+# POSITION - Keeps track of the line number, column number, and index
+######################################################################
+class Position:
+    def __init__(self, index, line, col, file_name, file_text):
+        self.index = index
+        self.line = line
+        self.col = col
+        self.file_name = file_name
+        self.file_text = file_text
 
-#class ExpectedCharError(Error):
- #   def __init__(self, pos_start, pos_end, details):
-  #      super().__init__(pos_start, pos_end, 'Expected Character', details)
+    # Move to the next index and update line and column numbers if necessary
 
-# Token types
+    def advance(self, current_char):
+        self.index += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.line = 1
+            self.col = 0
+
+        return self
+
+    # Create a copy of the current position
+    def copy(self):
+        return Position(self.index, self.line, self.col, self.file_name, self.file_text)
+
+
+######################################################################
+# TOKENS
+######################################################################
 
 TT_INT = 'INT'
 TT_FLOAT = 'FLOAT'
+TT_STRING = 'STRING'
+TT_IDENTIFIER = 'IDENTIFIER'
+TT_KEYWORD = 'KEYWORD'
 TT_PLUS = 'PLUS'
 TT_MINUS = 'MINUS'
 TT_MUL = 'MUL'
 TT_DIV = 'DIV'
+TT_EQ = 'EQ'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
+TT_EE = 'EE'
+TT_NE = 'NE'
+TT_LT = 'LT'
+TT_GT = 'GT'
+TT_LTE = 'LTE'
+TT_GTE = 'GTE'
+TT_COMMA = 'COMMA'
+TT_ARROW = 'ARROW'
+TT_NEWLINE = 'NEWLINE'
+TT_EOF = 'EOF'
 
-#CREATING THE TOKENS
+
+KEYWORDS = [
+    'VAR',
+    'AND',
+    'OR',
+    'NOT',
+    'IF',
+    'THEN',
+    'ELIF',
+    'ELSE',
+    'FOR',
+    'TO',
+    'FOR',
+    'WHILE',
+    'FUN',
+    'THEN',
+    'RETURN',
+    'CONTINUE',
+    'BREAK',
+    'END',
+]
+
+
 class Token:
-    def __init__(self, type, value=None):
-        self.type = type
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+        self.type = type_
         self.value = value
 
     def __repr__(self):
-        if self.value: return f'{self.type}:{self.value}'
+        if self.value:
+            return f'{self.type}:{self.value}'
         return f'{self.type}'
 
-#CREATING THE LEXER
 
+######################################################################
+# LEXER
+######################################################################
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, file_name, text):
+        self.file_name = file_name
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, file_name, text)
         self.current_char = None
         self.advance()
 
     def advance(self):
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance(self.current_char)
+        if self.pos.index < len(self.text):
+            self.current_char = self.text[self.pos.index]
+        else:
+            self.current_char = None
 
-#DEFINING THE TOKENS
     def make_tokens(self):
         tokens = []
 
         while self.current_char != None:
             if self.current_char in ' \t':
                 self.advance()
+            elif self.current_char == ';\n':
+                tokens.append(Token(TT_NEWLINE))
+                self.advance()
             elif self.current_char in DIGITS:
                 tokens.append(self.make_number())
+            elif self.current_char in LETTERS:
+                tokens.append(self.make_identifier())
+            elif self.current_char == '"':
+                tokens.append(self.make_string())
+            # To modify once parser is implemented
+            elif self.current_char in KEYWORDS:
+                tokens.append(self.make_keyword())
             elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
                 self.advance()
             elif self.current_char == '-':
-                tokens.append(Token(TT_MINUS))
+                tokens.append(self.make_minus_or_arrow())
                 self.advance()
             elif self.current_char == '*':
                 tokens.append(Token(TT_MUL))
@@ -94,25 +175,48 @@ class Lexer:
             elif self.current_char == '/':
                 tokens.append(Token(TT_DIV))
                 self.advance()
+            elif self.current_char == '=':
+                tokens.append(Token(TT_EQ))
+                self.advance()
             elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN))
                 self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
+            elif self.current_char == ',':
+                tokens.append(Token(TT_COMMA))
+                self.advance()
+            elif self.current_char == '!':
+                token, error = self.make_not_equals()
+                if error:
+                    return [], error
+                tokens.append(token)
+
+            elif self.current_char == '=':
+                tokens.append(self.make_equals())
+            elif self.current_char == '<':
+                tokens.append(self.make_less_than())
+            elif self.current_char == '>':
+                tokens.append(self.make_greater_than())
+
             else:
+                pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError("'" + char + "'")
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
 
+        tokens.append(Token(TT_EOF, pos_start=self.pos))
         return tokens, None
 
     def make_number(self):
         num_str = ''
         dot_count = 0
+
         while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
-                if dot_count == 1: break
+                if dot_count == 1:
+                    break
                 dot_count += 1
                 num_str += '.'
             else:
@@ -124,9 +228,115 @@ class Lexer:
         else:
             return Token(TT_FLOAT, float(num_str))
 
-    #RUN THE LEXER
-def run(text):
-    lexer = Lexer(text)
+    def make_string(self):
+        string = ''
+        pos_start = self.pos.copy()
+
+        escape_character = False
+
+        self.advance()
+
+        escape_characters = {
+            'n': '\n',
+            't': '\t'
+        }
+
+        while self.current_char != None and (self.current_char != '"' or escape_character):
+        # while self.current_char != None and self.current_char != '"':
+            if escape_character:
+                string += escape_characters.get(self.current_char, self.current_char)
+                escape_character = False
+            else:
+                if self.current_char == '\\':
+                    escape_character = True
+                else:
+                    string += self.current_char
+            self.advance()
+        return Token(TT_STRING, string, pos_start, self.pos)
+
+    def make_identifier(self):
+        identifier_str = ''
+        pos_start = self.pos.copy()
+
+        while self.current_char != None and self.current_char in LETTERS_DIGITS + '_':
+            identifier_str += self.current_char
+            self.advance()
+
+        token_type = TT_KEYWORD if identifier_str in KEYWORDS else TT_IDENTIFIER
+        return Token(token_type, identifier_str, pos_start, self.pos)
+
+    def make_minus_or_arrow(self):
+        token_type = TT_MINUS
+        pos_start = self.pos.copy()
+
+        self.advance()
+        if self.current_char == '>':
+            self.advance()
+            token_type = TT_ARROW
+
+        return Token(token_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_not_equals(self):
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            return Token(TT_NE, pos_start=pos_start, pos_end=self.pos), None
+
+        self.advance()
+        return None, ExpectedCharError(pos_start, self.pos, "'=' (after '!')")
+
+    def make_equals(self):
+        token_type = TT_EQ
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            token_type = TT_EE
+
+        return Token(token_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_less_than(self):
+        token_type = TT_LT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            token_type = TT_LTE
+
+        return Token(token_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_greater_than(self):
+        token_type = TT_GT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            token_type = TT_GTE
+
+        return Token(token_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_keyword(self):
+        keyword_str = ''
+        pos_start = self.pos.copy()
+
+        while self.current_char != None and self.current_char in LETTERS_DIGITS + '_':
+            keyword_str += self.current_char
+            self.advance()
+
+        return Token(TT_KEYWORD, keyword_str, pos_start, self.pos)
+
+######################################################################
+# RUN
+######################################################################
+
+
+def run(file_name, text):
+    lexer = Lexer(file_name, text)
     tokens, error = lexer.make_tokens()
 
     return tokens, error
--- shell.py
+++ shell.py
@@ -2,9 +2,9 @@ import basic
 
 while True:
     text = input('Mini> ')
-    result, error = basic.run(text)
+    result, error = basic.run('<stdin>', text)
 
     if error: 
         print(error.as_string())
-    else: 
+    else:  
         print(result)
